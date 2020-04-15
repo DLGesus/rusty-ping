26a06d08c7bdabe7ebb077352150d0a01676c169
DLGesus <cmd170001@utdallas.edu>
2020-04-15 21:56:44 +0000

    Updated readme and added comments

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
 # rusty-ping
+
+A Rust built Ping CLI application that supports Ipv4 and hostname addressing. This application was developed for the Cloudflare systems internship.
+
+### Commandline Usage
+
+```
+$ cargo run -- 1.1.1.1 --ttl=128
+$ ./target/debug/rusty-ping google.com --ttl=4
+```
+
+### Output
+
+This is the output when pinging google.com with a TTL set to 59
+
+```
+$ ./target/debug/rusty-ping google.com --ttl=59
+Pinging 172.217.12.174 with TTL=59
+Reply from 172.217.12.174: bytes=28 time=1ms TTL=56
+        Packets Sent = 1, Received = 1, Lost = 0 (0% loss)
+
+Pinging 172.217.12.174 with TTL=59
+Reply from 172.217.12.174: bytes=28 time=1ms TTL=56
+        Packets Sent = 2, Received = 2, Lost = 0 (0% loss)
+
+Pinging 172.217.12.174 with TTL=59
+Reply from 172.217.12.174: bytes=28 time=1ms TTL=56
+        Packets Sent = 3, Received = 3, Lost = 0 (0% loss)
+```
+
+This is the output when pinging 1.1.1.1 with a TTL set to 2. Notice that because it either timed out or received a time exceeded response, it will increase the TTL by 1 to attempt a connection.
+
+```
+$ ./target/debug/rusty-ping 1.1.1.1 --ttl=2
+Pinging 1.1.1.1 with TTL=2
+Packet Error: Time exceeded.
+        Packets Sent = 1, Received = 0, Lost = 1 (100% loss)
+
+Pinging 1.1.1.1 with TTL=3
+Packet timed out
+        Packets Sent = 2, Received = 0, Lost = 2 (100% loss)
+
+Pinging 1.1.1.1 with TTL=4
+Packet timed out
+        Packets Sent = 3, Received = 0, Lost = 3 (100% loss)
+
+Pinging 1.1.1.1 with TTL=5
+Packet timed out
+        Packets Sent = 4, Received = 0, Lost = 4 (100% loss)
+
+Pinging 1.1.1.1 with TTL=6
+Packet timed out
+        Packets Sent = 5, Received = 0, Lost = 5 (100% loss)
+
+Pinging 1.1.1.1 with TTL=7
+Reply from 1.1.1.1: bytes=28 time=1ms TTL=59
+        Packets Sent = 6, Received = 1, Lost = 5 (83% loss)
+
+Pinging 1.1.1.1 with TTL=7
+Reply from 1.1.1.1: bytes=28 time=1ms TTL=59
+        Packets Sent = 7, Received = 2, Lost = 5 (71% loss)
+```
--- src/main.rs
+++ src/main.rs
@@ -20,10 +20,10 @@ use std::thread;
 fn main() {
     let matches = App::new("rusty-ping")
                     .version("1.0")
-                    .about("Sample Text")
+                    .about("Ping CLI application built for Cloudflare's systems programming challenge")
                     .author("Chris DeLaGarza")
                     .arg(Arg::with_name("IP")
-                        .help("IPv4 or IPv6 address to ping")
+                        .help("IPv4 or IPv6* address to ping")
                         .required(true)
                         .index(1))
                     .arg(Arg::with_name("TTL")
@@ -43,6 +43,8 @@ fn main() {
             v4_ping(ip, ttl);
         },
         IpAddr::V6(ip) => {
+            // Pinging IPv6 using the Pnet rust crate does not seem to be properly supported.
+            // Although the code is written to work, this will throw an OS error.
             v6_ping(ip, ttl);
         }
     }
@@ -62,27 +64,29 @@ fn v4_ping(ip: Ipv4Addr, mut ttl: u8) {
         let ping_packet = build_v4_packet(ip_buf, icmp_buf, ip, sequence_number, ttl);
 
         println!("Pinging {} with TTL={}", ip, ping_packet.get_ttl());
-        sender.send_to(ping_packet, IpAddr::V4(ip)).unwrap();
         let now = Instant::now();
+        sender.send_to(ping_packet, IpAddr::V4(ip)).unwrap();
         total_sent += 1;
         // Get the incoming packet
         let timeout = Duration::from_secs(2);
         loop {
             if let Some((packet, addr)) = iter.next_with_timeout(timeout).unwrap() {
-                let elasped = now.elapsed().as_micros();
-                let elasped_str = match elasped < 1000 {
-                    true => format!("<1"),
-                    false => format!("{}", elasped / 1000)
-                };
                 let icmp_packet = EchoReplyPacket::new(packet.payload()).unwrap();
+                // Ensure its the correct packet
                 if icmp_packet.get_sequence_number() == sequence_number {
                     if icmp_packet.get_icmp_type() == IcmpTypes::EchoReply {
+                        let elasped = now.elapsed().as_micros();
+                        let elasped_str = match elasped < 1000 {
+                            true => format!("<1"),
+                            false => format!("{}", elasped / 1000)
+                        };
                         total_sucessful += 1;
                         println!("Reply from {}: bytes={} time={}ms TTL={}", addr, packet.get_total_length(), elasped_str, packet.get_ttl());
                     }
                     else if icmp_packet.get_icmp_type() == IcmpTypes::TimeExceeded {
                         println!("Packet Error: Time exceeded.");
-                        ttl += 1;
+                        // Attempt to reach the destination by increasing the time to live
+                        ttl += if ttl == 255 { 0 } else { 1 };
                     }
                     else {
                         println!("Received reply, but unhandled message");
@@ -93,7 +97,8 @@ fn v4_ping(ip: Ipv4Addr, mut ttl: u8) {
             }
             else {
                 println!("Packet timed out");
-                ttl += 1;
+                // Packet may have time exceeded without a return message. Increase time to live to be safe.
+                ttl += if ttl == 255 { 0 } else { 1 }; 
                 break;
             }
         }
@@ -107,6 +112,52 @@ fn v4_ping(ip: Ipv4Addr, mut ttl: u8) {
     }
 }
 
+fn build_icmp_packet<'x>(icmp_buffer: &'x mut [u8], sequence_number: u16, is_v4: bool) -> MutableEchoRequestPacket<'x> {
+    let mut icmp_packet = MutableEchoRequestPacket::new(icmp_buffer).unwrap();
+    icmp_packet.set_sequence_number(sequence_number);
+    icmp_packet.set_identifier(44); // Magic Number
+    if is_v4 {
+        icmp_packet.set_icmp_type(IcmpTypes::EchoRequest); // Type = 0 (Echo Request)
+        icmp_packet.set_icmp_code(IcmpCode::new(0));
+    }
+    else {
+        let mut icmpv6_packet = MutableIcmpv6Packet::new(icmp_packet.packet_mut()).unwrap();
+        icmpv6_packet.set_icmpv6_type(Icmpv6Types::EchoRequest); // Type = 128 (Echo Request)
+        icmpv6_packet.set_icmpv6_code(Icmpv6Code::new(0));
+    }
+    let icmp_checksum = checksum(&IcmpPacket::new(icmp_packet.packet()).unwrap());
+    icmp_packet.set_checksum(icmp_checksum);
+    icmp_packet
+}
+
+fn build_v4_packet<'x>(ip_buffer: &'x mut [u8], icmp_buffer: &'x mut [u8], dest: Ipv4Addr, sequence_number: u16, ttl:u8) -> MutableIpv4Packet<'x> {
+    // Build ip packet
+    let mut ip_packet = MutableIpv4Packet::new(ip_buffer).unwrap();
+    ip_packet.set_version(4);           // Ipv4's version number is 4
+    ip_packet.set_header_length(5);     // 20 bytes
+    ip_packet.set_total_length(28);     // Size of ipv4 (20) + size of icmp header (8)
+    ip_packet.set_identification(44);   // Magic number
+    ip_packet.set_ttl(ttl);
+    ip_packet.set_next_level_protocol(IpNextHeaderProtocols::Icmp);
+    ip_packet.set_destination(dest);
+    let ip_checksum = ipv4_checksum(&Ipv4Packet::new(ip_packet.packet()).unwrap());
+    ip_packet.set_checksum(ip_checksum);
+
+    // Build the icmp packet
+    let mut icmp_packet = build_icmp_packet(icmp_buffer, sequence_number, true);
+
+    // Place the icmp packet into the ip packets's payload
+    ip_packet.set_payload(icmp_packet.packet_mut());
+    ip_packet
+}
+
+/**
+ * This is the attempt to implement the Ipv6 version of the ping application.
+ * The theory and code setup should theoretically work, however, it would seem
+ * that the pnet crate used does not properly support ipv6 as attempting to send
+ * a ipv6 packet results in a OS error with code 97 ("Address familty not supported 
+ * by protocol").
+ */
 fn v6_ping(ip: Ipv6Addr, ttl: u8) {
     // Setup the channel for sending and accepting icmp packages
     let (mut sender, mut receiver) = transport_channel(4096, Layer3(IpNextHeaderProtocols::Icmpv6)).unwrap();
@@ -163,45 +214,6 @@ fn v6_ping(ip: Ipv6Addr, ttl: u8) {
         });
 
     }
-}
-
-fn build_icmp_packet<'x>(icmp_buffer: &'x mut [u8], sequence_number: u16, is_v4: bool) -> MutableEchoRequestPacket<'x> {
-    let mut icmp_packet = MutableEchoRequestPacket::new(icmp_buffer).unwrap();
-    icmp_packet.set_sequence_number(sequence_number);
-    icmp_packet.set_identifier(44); // Magic Number
-    if is_v4 {
-        icmp_packet.set_icmp_type(IcmpTypes::EchoRequest); // Type = 0 (Echo Request)
-        icmp_packet.set_icmp_code(IcmpCode::new(0));
-    }
-    else {
-        let mut icmpv6_packet = MutableIcmpv6Packet::new(icmp_packet.packet_mut()).unwrap();
-        icmpv6_packet.set_icmpv6_type(Icmpv6Types::EchoRequest); // Type = 128 (Echo Request)
-        icmpv6_packet.set_icmpv6_code(Icmpv6Code::new(0));
-    }
-    let icmp_checksum = checksum(&IcmpPacket::new(icmp_packet.packet()).unwrap());
-    icmp_packet.set_checksum(icmp_checksum);
-    icmp_packet
-}
-
-fn build_v4_packet<'x>(ip_buffer: &'x mut [u8], icmp_buffer: &'x mut [u8], dest: Ipv4Addr, sequence_number: u16, ttl:u8) -> MutableIpv4Packet<'x> {
-    // Build ip packet
-    let mut ip_packet = MutableIpv4Packet::new(ip_buffer).unwrap();
-    ip_packet.set_version(4);
-    ip_packet.set_header_length(5);
-    ip_packet.set_total_length(28);
-    ip_packet.set_identification(44);
-    ip_packet.set_ttl(ttl);
-    ip_packet.set_next_level_protocol(IpNextHeaderProtocols::Icmp);
-    ip_packet.set_destination(dest);
-    let ip_checksum = ipv4_checksum(&Ipv4Packet::new(ip_packet.packet()).unwrap());
-    ip_packet.set_checksum(ip_checksum);
-
-    // Build the icmp packet
-    let mut icmp_packet = build_icmp_packet(icmp_buffer, sequence_number, true);
-
-    // Place the icmp packet into the ip packets's payload
-    ip_packet.set_payload(icmp_packet.packet_mut());
-    ip_packet
 }
 
 fn build_v6_packet<'x>(ip_buffer: &'x mut [u8], icmp_buffer: &'x mut [u8], dest: Ipv6Addr, sequence_number: u16, ttl: u8) -> MutableIpv6Packet<'x> {
